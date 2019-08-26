add77c7749ea9bba46db9c72706f4ef813f41d53
Sahil Khokhar <sahilkhokhar505@gmail.com>
2019-08-26 15:31:49 +0530

    error handler up

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const SlackBot = require('slackbots');
 const axios = require('axios');
 
- const bot = new SlackBot({
+const bot = new SlackBot({
   token: 'xoxb-740252521542-737604450292-Mnskuk7QHXQqB4WsSkRI3O4k',
   name: 'jokebot'
- });
+});
 
- // Start Handler
- bot.on('start', () => {
-   const params = {
-     icon_emoji: ':laughing:'
-   }
+// Start Handler
+bot.on('start', () => {
+  const params = {
+    icon_emoji: ':laughing:'
+  }
 
-   bot.postMessageToChannel('general', 'Get ready to burst into tears with @jokebot', params);
- });
+  bot.postMessageToChannel('general', 'Get ready to burst into tears with @jokebot', params);
+});
+
+// Error Handler
+bot.on('error', (err) => console.log(err));
